ed449b6352e0bf259850efa3587ac3bdf6003756
mkrej <mkrej@wiml.waw.pl>
2018-08-16 15:04:24 +0200

    ref

--- sos.R
+++ sos.R
@@ -1,4 +1,11 @@
+#
+# sos.R
+#
+#  Created on: 17 Apr 2018
+#      Author: Mariusz Krej
+#
 
+    
 cplxpair = function(inp)
 {
     thresh = 100 * .Machine$double.eps
@@ -177,6 +184,99 @@ tf2sos = function(tf)
     p = roots(tf$a)
     k = tf$b[1] / tf$a[1]
     zp2sos(z, p, k)
+}
+
+
+
+sectionFormII = function(state, section, value)
+{
+    b = section[1:3]
+    a = section[4:6]
+    state[1] = value - a[2] * state[2] - a[3] * state[3]
+    res = sum(state * b)
+    state[2:3] = state[1:2]
+    list(res = res, state = state)
+}
+
+
+sectionFormIItrans = function(state, section, value)
+{
+    b = section[1:3]
+    a = section[4:6]
+    
+    res =      b[1] * value + state[2];
+    state[2] = b[2] * value - a[2] * res + state[3];
+    state[3] = b[3] * value - a[3] * res;
+    
+    list(res = res, state = state)
+}
+# debugonce(sectionFormIItrans)
+
+
+
+sosFilter = function(sos, x, zi)
+{
+    nsec = nrow(sos$sos)
+    
+    state = matrix(rep(0, 3 * nsec), nrow = nsec)
+    
+    if(!missing(zi))
+    {
+        state[, 2:3] = zi
+    }
+    
+    y = x * 0
+    for(i in 1:length(x))
+    {          
+        value = x[i]
+        for (r in 1:nsec)
+        {
+            cmp = sectionFormIItrans(state = state[r,], section = sos$sos[r,], value)
+            value = cmp$res
+            state[r,] = cmp$state
+        }
+        y[i] = value
+    }
+    #print(state)
+    y * sos$g
+}
+
+
+# wg. scipy.signal.lfilter_zi(b, a)
+biquad_zi = function(b, a)
+{
+    companT = matrix(c(-a[2:3], c(1, 0)), nrow = 2)
+    ImA = diag(2) - companT
+    B = b[2:3] - a[2:3] * b[1]
+    solve(ImA, B)
+}
+
+
+
+sos_zi = function(sos)
+{
+    nsec = nrow(sos$sos)
+    zi = matrix(rep(0, 2 * nsec), nrow = nsec)
+    
+    scale = 1
+    
+    for (r in 1:nsec)
+    {
+        section = sos$sos[r, ]
+        b = section[1:3]
+        a = section[4:6]
+        zi[r, ] = scale * biquad_zi(b, a)
+        scale = scale * sum(b) / sum(a)
+    }
+    zi
+}
+
+hackFilter = function(filt, a, x, init.x=c())
+{
+    x <- stats::filter(c(init.x, x), filt / a[1], sides = 1)
+    x <- na.omit(x, filt / a[1] , sides = 1)
+    x <- stats::filter(x, -a[-1] / a[1], method = "recursive")
+    x
 }
 
 
--- sosTests.R
+++ sosTests.R
@@ -71,99 +71,6 @@ tf2sos(tf)
 
 
 
-sectionFormII = function(state, section, value)
-{
-    b = section[1:3]
-    a = section[4:6]
-    state[1] = value - a[2] * state[2] - a[3] * state[3]
-    res = sum(state * b)
-    state[2:3] = state[1:2]
-    list(res = res, state = state)
-}
-
-
-sectionFormIItrans = function(state, section, value)
-{
-    b = section[1:3]
-    a = section[4:6]
-
-    res =      b[1] * value + state[2];
-    state[2] = b[2] * value - a[2] * res + state[3];
-    state[3] = b[3] * value - a[3] * res;
-
-    list(res = res, state = state)
-}
-# debugonce(sectionFormIItrans)
-
-
-
-sosFilter = function(sos, x, zi)
-{
-   nsec = nrow(sos$sos)
-   
-   state = matrix(rep(0, 3 * nsec), nrow = nsec)
-   
-   if(!missing(zi))
-   {
-       state[, 2:3] = zi
-   }
-
-   y = x * 0
-   for(i in 1:length(x))
-   {          
-       value = x[i]
-       for (r in 1:nsec)
-       {
-           cmp = sectionFormIItrans(state = state[r,], section = sos$sos[r,], value)
-           value = cmp$res
-           state[r,] = cmp$state
-       }
-       y[i] = value
-   }
-   #print(state)
-   y * sos$g
-}
-
-
-# wg. scipy.signal.lfilter_zi(b, a)
-biquad_zi = function(b, a)
-{
-    companT = matrix(c(-a[2:3], c(1, 0)), nrow = 2)
-    ImA = diag(2) - companT
-    B = b[2:3] - a[2:3] * b[1]
-    solve(ImA, B)
-}
-
-
-
-sos_zi = function(sos)
-{
-    nsec = nrow(sos$sos)
-    zi = matrix(rep(0, 2 * nsec), nrow = nsec)
-
-    scale = 1
-    
-    for (r in 1:nsec)
-    {
-        section = sos$sos[r, ]
-        b = section[1:3]
-        a = section[4:6]
-        zi[r, ] = scale * biquad_zi(b, a)
-        scale = scale * sum(b) / sum(a)
-    }
-    zi
-}
-
-hackFilter = function(filt, a, x, init.x=c())
-{
-    x <- stats::filter(c(init.x, x), filt / a[1], sides = 1)
-    x <- na.omit(x, filt / a[1] , sides = 1)
-    x <- stats::filter(x, -a[-1] / a[1], method = "recursive")
-    x
-}
-
-
-
 #---------------------
 #analiza 1 sekcji
 tf = butter(2, 1 / (fs/2), type = "high")
@@ -186,6 +93,9 @@ lines(flt3, type = 'l', col = 'black')
 
 flt4 = sosFilter(prepSos, x, zi = x[1] *biquad_zi(tf$b, tf$a))
 lines(flt4, type = 'l', col = 'black')
+
+
+
 
 # ---------------
 # 2 sekcje
