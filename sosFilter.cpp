f8c00d532d49f08920d4c57ac1a6adbf725f24f7
mkrej <mkrej>
2019-03-29 11:40:39 +0100

    state ma 2 elementy / sekcję, ref i clean

--- sosFilter.cpp
+++ sosFilter.cpp
@@ -7,29 +7,20 @@ double sectionFormIItrans(double *state, double section[6], double value)
 {
   double *b = section;
   double *a = section + 3;
-  double res = b[0] * value + state[1];
-  state[1] = b[1] * value - a[1] * res + state[2];
-  state[2] = b[2] * value - a[2] * res;
-  
+  double res = b[0] * value + state[0];
+  state[0] = b[1] * value - a[1] * res + state[1];
+  state[1] = b[2] * value - a[2] * res;
   return res;
 }
 
 
-void sosFilter_c(double *sos, int nsec, double gain, double *x, int nx, double *zi, double *y) 
+void sosFilter_c(double *x, int nx, double *state, double *sos, int nsec, double gain, double *y) 
 {
-  
-  double stateVec[nsec * 3];
-  double* state = &stateVec[0];
-  
-  for (int r = 0; r < nsec; r++)
-    for(int i = 0; i < 2; i++)
-      state[r * 3 + 1 + i] = zi == 0 ? 0 : zi[r * 2 + i];
-  
   for(int i = 0; i < nx; i++)
   {          
     double value = x[i];
     for (int r = 0; r < nsec; r++)
-       value = sectionFormIItrans(state + r * 3, sos + r * 6, value);
+       value = sectionFormIItrans(state + r * 2, sos + r * 6, value);
     y[i] = value * gain;
   }
   
@@ -42,17 +33,14 @@ void sosFilter_c(double *sos, int nsec, double gain, double *x, int nx, double *
 NumericVector sosFilter_cpp(NumericMatrix sos, double gain, NumericVector x, NumericMatrix zi) 
 {
   int nx = x.size();
-  
-  NumericVector y = NumericVector(nx);
-  
   int nsec = sos.nrow();
-
+  
   sos = transpose(sos);
   zi = transpose(zi);
-  
-  NumericVector stateVec = NumericVector(nsec * 3);
 
-  sosFilter_c(&sos[0], nsec, gain, &x[0], nx, &zi[0], &y[0]);
+  NumericVector y = NumericVector(nx);
+  
+  sosFilter_c(&x[0], nx, &zi[0], &sos[0], nsec, gain, &y[0]);
 
   return y;
 }
