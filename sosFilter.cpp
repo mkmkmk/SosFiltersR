4d811ab73477806a7d58b0292c0636f7948594fd
mkrej <mkrej>
2019-03-29 11:33:11 +0100

    cl

--- sosFilter.cpp
+++ sosFilter.cpp
@@ -7,7 +7,6 @@ double sectionFormIItrans(double *state, double section[6], double value)
 {
   double *b = section;
   double *a = section + 3;
-  
   double res = b[0] * value + state[1];
   state[1] = b[1] * value - a[1] * res + state[2];
   state[2] = b[2] * value - a[2] * res;
@@ -24,26 +23,13 @@ void sosFilter_c(double *sos, int nsec, double gain, double *x, int nx, double *
   
   for (int r = 0; r < nsec; r++)
     for(int i = 0; i < 2; i++)
-      state[r * 3 + 1 + i]  = zi[r * 2 + i];
-  
+      state[r * 3 + 1 + i] = zi == 0 ? 0 : zi[r * 2 + i];
   
   for(int i = 0; i < nx; i++)
   {          
     double value = x[i];
     for (int r = 0; r < nsec; r++)
-    {
-      //NumericVector sectRow = NumericVector(6);
-      //for(int k = 0; k < 6; k++)
-      //   sectRow[k] = sos(r, k);
-      //double * sectRow = sos + r * 6;
-      //if(i==0)
-      //{
-      //  for(int k = 0; k < 6; k++)
-      //    printf("%g, ", sectRow[k]);
-      //  printf("\n");
-      //}
-      value = sectionFormIItrans(state + r * 3, sos + r * 6, value);
-    }
+       value = sectionFormIItrans(state + r * 3, sos + r * 6, value);
     y[i] = value * gain;
   }
   
@@ -60,49 +46,14 @@ NumericVector sosFilter_cpp(NumericMatrix sos, double gain, NumericVector x, Num
   NumericVector y = NumericVector(nx);
   
   int nsec = sos.nrow();
-  
-  
+
   sos = transpose(sos);
   zi = transpose(zi);
   
   NumericVector stateVec = NumericVector(nsec * 3);
-  
-  
-  sosFilter_c(&sos[0], nsec, gain, &x[0], nx, &zi[0], &y[0]);
-  
-  /*
-  double* state = &stateVec[0];
-  
-  for (int r = 0; r < nsec; r++)
-    for(int i = 0; i < 2; i++)
-      state[r * 3 + 1 + i]  = zi(i, r);
 
-  
-  
-  
-  for(int i = 0; i < nx; i++)
-  {          
-    double value = x[i];
-    for (int r = 0; r < nsec; r++)
-    {
-      //NumericVector sectRow = NumericVector(6);
-      //for(int k = 0; k < 6; k++)
-      //   sectRow[k] = sos(r, k);
-      double * sectRow = &sos[0] + r * 6;
-      
-      //if(i==0)
-      //{
-      //  for(int k = 0; k < 6; k++)
-      //    printf("%g, ", sectRow[k]);
-      //  printf("\n");
-      //}
-      
-      value = sectionFormIItrans(state + r * 3, &sectRow[0], value);
-    }
-    y[i] = value * gain;
-  }
-  */
-  
+  sosFilter_c(&sos[0], nsec, gain, &x[0], nx, &zi[0], &y[0]);
+
   return y;
 }
 
