3d5e14ffe6044f1077ae87d446dce9aee1102c2a
mkrej <mkrej>
2019-07-15 11:37:37 +0100

    generowanie kodu ze współczynnikami dla filtrów Q16.16

--- genDecFilter.R
+++ genDecFilter.R
@@ -9,17 +9,52 @@ source("sos.R")
 sourceCpp('sosFilter.cpp')
 
 
-p = 2
-q = 3
+p = 1
+q = 4
 dec_tf = butter(8, 0.8 / max (p, q))
 dec_sos = tf2sos(dec_tf)
-sosCoef = as.vector(t(dec_sos$sos))
-cat("\n\n//-------\n")
-cat(sprintf("// resamp filter %s/%s\n", p, q))
-cat(sprintf("#define RESAMP_P (%g)\n", p))
-cat(sprintf("#define RESAMP_Q (%g)\n", q))
-cat(sprintf("#define N_SEC (%g)\n", nrow(dec_sos$sos)))
-cat(sprintf("#define GAIN (%.6g)\n", dec_sos$g))
-cat(sprintf("double sos[] = { %s };\n", paste(sprintf("%.10g", sosCoef), collapse = ", ")))
-cat("\n")
+
+
+if(F)
+{
+    sosCoef = as.vector(t(dec_sos$sos))
+    cat("\n\n//-------\n")
+    cat(sprintf("// resamp filter %s/%s\n", p, q))
+    cat(sprintf("#define RESAMP_P (%g)\n", p))
+    cat(sprintf("#define RESAMP_Q (%g)\n", q))
+    cat(sprintf("#define N_SEC (%g)\n", nrow(dec_sos$sos)))
+    cat(sprintf("#define GAIN (%.6g)\n", dec_sos$g))
+    cat(sprintf("double sos[] = { %s };\n", paste(sprintf("%.10g", sosCoef), collapse = ", ")))
+    cat("\n")
+    
+} else # Q16.16 
+{
+    # rozdzielenie gains
+    gains = rep(dec_sos$g ^ (1 / nrow(dec_sos$sos)), nrow(dec_sos$sos))
+    
+    # włączenie gains do sos
+    for(r in 1:nrow(dec_sos$sos))
+    {
+        dec_sos$sos[r, 1:3] = dec_sos$sos[r, 1:3] * gains[r]
+        gains[r] = 1
+    }
+    dec_sos$g = 1
+
+    sosCoef = as.vector(t(dec_sos$sos))
+    sosCoef = trunc(sosCoef * 2^16)
+    
+    br = range(log2(abs(sosCoef)))
+    cat(sprintf("bit range: [%.4g, %.4g]\n", br[1], br[2]))
+    
+    cat("\n\n//-------\n")
+    cat(sprintf("// resamp filter %s/%s\n", p, q))
+    cat(sprintf("#define RESAMP_P (%g)\n", p))
+    cat(sprintf("#define RESAMP_Q (%g)\n", q))
+    cat(sprintf("#define N_SEC (%g)\n", nrow(dec_sos$sos)))
+    cat(sprintf("int32_t sos[] = { %s };\n", paste(sprintf("%.10g", sosCoef), collapse = ", ")))
+    cat("\n")
+    
+}
+
+
 
