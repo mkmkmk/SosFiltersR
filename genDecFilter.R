3719c792fa7bcd0de440f2025926ab86a8b74139
mkrej <mk>
2019-09-19 18:41:30 +0200

    cd drukowanie filtrów

--- genDecFilter.R
+++ genDecFilter.R
@@ -10,16 +10,25 @@ sourceCpp('sosFilter.cpp')
 
 
 p = 1
+
+q = 16/5
 q = 4
-dec_tf = butter(8, 0.8 / max (p, q))
+q = 2
+order = 8
+gainMul = 1
+
+dec_tf = butter(order, 0.8 / max (p, q))
 dec_sos = tf2sos(dec_tf)
+
+# korekcja gain-a
+dec_sos$g = dec_sos$g * gainMul
 
 
 if(F)
 {
     sosCoef = as.vector(t(dec_sos$sos))
     cat("\n\n//-------\n")
-    cat(sprintf("// resamp filter %s/%s\n", p, q))
+    cat(sprintf("// float resamp filter %d/%d, order %d, gain x%g\n", p, q, order, gainMul))
     cat(sprintf("#define RESAMP_P (%g)\n", p))
     cat(sprintf("#define RESAMP_Q (%g)\n", q))
     cat(sprintf("#define N_SEC (%g)\n", nrow(dec_sos$sos)))
@@ -41,17 +50,17 @@ if(F)
     dec_sos$g = 1
 
     sosCoef = as.vector(t(dec_sos$sos))
-    sosCoef = trunc(sosCoef * 2^16)
+    sosCoef = round(sosCoef * 2^16)
     
     br = range(log2(abs(sosCoef)))
     cat(sprintf("bit range: [%.4g, %.4g]\n", br[1], br[2]))
     
     cat("\n\n//-------\n")
-    cat(sprintf("// resamp filter %s/%s\n", p, q))
+    cat(sprintf("// Q.16 resamp filter %d/%d, order %d, gain x%g\n", p, q, order, gainMul))
     cat(sprintf("#define RESAMP_P (%g)\n", p))
     cat(sprintf("#define RESAMP_Q (%g)\n", q))
-    cat(sprintf("#define N_SEC (%g)\n", nrow(dec_sos$sos)))
-    cat(sprintf("int32_t sos[] = { %s };\n", paste(sprintf("%.10g", sosCoef), collapse = ", ")))
+    cat(sprintf("#define SOS_N_SEC (%g)\n", nrow(dec_sos$sos)))
+    cat(sprintf("int32_t SOS[] = { %s };\n", paste(sprintf("%.10g", sosCoef), collapse = ", ")))
     cat("\n")
     
 }
